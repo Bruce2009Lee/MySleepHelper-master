c4ee518563edace00a66dd72da9fc602bd0c6b27
huaishuiguwu <zhonglianli2009@163.com>
2017-12-28 23:25:19 +0800

    modify theme activity

--- app/src/main/java/com/example/developerhaoz/sleephelper/recyclerview/BaseActivity.java
+++ app/src/main/java/com/example/developerhaoz/sleephelper/recyclerview/BaseActivity.java
@@ -5,54 +5,52 @@ import android.os.Bundle;
 
 import com.example.developerhaoz.sleephelper.R;
 
+import static com.example.developerhaoz.sleephelper.R.style.BiLiPinkTheme;
+
 public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        initTheme();
+    }
 
-        private void initTheme(){
+    private void initTheme(){
 
-            //TODO:
+        //TODO:
 //            int themeId = MyMusicUtil.getTheme(BaseActivity.this);
-            int themeId = BiLiPinkTheme;
-            switch (themeId){
-                default:
-                case 0:
-                    setTheme(R.style.BiLiPinkTheme);
-                    break;
-                case 1:
-                    setTheme(R.style.ZhiHuBlueTheme);
-                    break;
-                case 2:
-                    setTheme(R.style.KuAnGreenTheme);
-                    break;
-                case 3:
-                    setTheme(R.style.CloudRedTheme);
-                    break;
-                case 4:
-                    setTheme(R.style.TengLuoPurpleTheme);
-                    break;
-                case 5:
-                    setTheme(R.style.SeaBlueTheme);
-                    break;
-                case 6:
-                    setTheme(R.style.GrassGreenTheme);
-                    break;
-                case 7:
-                    setTheme(R.style.CoffeeBrownTheme);
-                    break;
-                case 8:
-                    setTheme(R.style.LemonOrangeTheme);
-                    break;
-                case 9:
-                    setTheme(R.style.StartSkyGrayTheme);
-                    break;
-                case 10:
-                    setTheme(R.style.NightModeTheme);
-                    break;
-            }
+        int themeId = BiLiPinkTheme;
+        switch (themeId){
+            default:
+            case 0:
+                setTheme(BiLiPinkTheme);
+                break;
+            case 1:
+                setTheme(R.style.ZhiHuBlueTheme);
+                break;
+            case 2:
+                setTheme(R.style.KuAnGreenTheme);
+                break;
+            case 3:
+                setTheme(R.style.CloudRedTheme);
+                break;
+            case 4:
+                setTheme(R.style.TengLuoPurpleTheme);
+                break;
+            case 5:
+                setTheme(R.style.SeaBlueTheme);
+                break;
+            case 6:
+                setTheme(R.style.GrassGreenTheme);
+                break;
+            case 7:
+                setTheme(R.style.CoffeeBrownTheme);
+                break;
+            case 8:
+                setTheme(R.style.LemonOrangeTheme);
+                break;
         }
     }
+
 }
